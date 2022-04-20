fbe146539c2c09edcc198239a8dd268bf48ab7f4
Ari <ari@ceiphr.com>
2022-04-20 15:32:18 -0400

    Finished initial version of website.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
-import 'katex/dist/katex.min.css';
+import "../styles/globals.css";
+import "../styles/prism.css";
+import "katex/dist/katex.min.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
